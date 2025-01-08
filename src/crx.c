0cbfb65428a58ddb97b7670ecaffe583f2bf942f
johndoesstuff <johndoesstuff03@gmail.com>
2025-01-07 22:02:20 -0500

    blehh im so silly blehh implicit declarations

--- src/crx.c
+++ src/crx.c
@@ -158,11 +158,11 @@ Token* Tokenize(const char *regex, int *tokCount) {
 	return tokens;
 }
 
-Token *Peek(const Token* tokens, int *position) {
+const Token *Peek(const Token* tokens, int *position) {
 	return &tokens[*position];
 }
 
-Token *Consume(TokenType type, const Token* tokens, int *position) {
+const Token *Consume(TokenType type, const Token* tokens, int *position) {
 	if (Peek(tokens, position)->type == type) {
 		return &tokens[(*position)++];
 	} else {
@@ -171,66 +171,15 @@ Token *Consume(TokenType type, const Token* tokens, int *position) {
 	}
 }
 
-ASTNode *Parse(const Token* tokens, int tokenCount) { //shoutout https://github.com/kean/Regex/blob/main/grammar.ebnf
-	const Token* token = tokens;
-	ASTNode *regex = malloc(sizeof(ASTNode));
-	int position = 0;
-	regex->type = NODE_REGEX;
-	regex->left = ConsumeExpression(tokens, tokenCount, &position);
-	return regex;
-}
-
-ASTNode *ConsumeExpression(const Token* tokens, int tokenCount, int *position) {
+ASTNode *ConsumeMatchItem(const Token* tokens, int tokenCount, int *position) {
 	int startPos = *position;
 	if (*position >= tokenCount) {
 		return NULL;
 	}
 	ASTNode *node = malloc(sizeof(ASTNode));
-	node->type = NODE_EXPRESSION;
-	node->left = ConsumeSubExpression(tokens, tokenCount, position);
-	if (!node->left) {
-		free(node);
-		*position = startPos;
-		return NULL;
-	}
-	if (Peek(tokens, position).type == TOKEN_ALTERNATOR) {
-		Consume(TOKEN_ALTERNATOR, tokens, position);
-		node->right = ConsumeExpression(tokens, tokenCount, position);
-	}
-	return node;
-}
-
-ASTNode *ConsumeSubExpression(const Token* tokens, int tokenCount, int *position) {
-	int startPos = *position;
-	if (*position >= tokenCount) {
-		return NULL;
-	}
-	ASTNode *node = malloc(sizeof(ASTNode));
-	node->type = NODE_SUBEXPRESSION;
-	node->left = ConsumeSubExpressionItem(tokens, tokenCount, position);
-	if (!node->left) {
-		free(node);
-		*position = startPos;
-		return NULL;
-	}
-	node->right = ConsumeSubExpression(tokens, tokenCount, position);
-	return node;
-}
-
-ASTNode *ConsumeSubExpressionItem(const Token* tokens, int tokenCount, int *position) {
-	int startPos = *position;
-	if (*position >= tokenCount) {
-		return NULL;
-	}
-	ASTNode *node = malloc(sizeof(ASTNode));
-	node = ConsumeMatch(tokens, tokenCount, position);
-	if (!node) node = ConsumeGroup(tokens, tokenCount, position);
-	if (!node) node = ConsumeAnchor(tokens, tokenCount, position);
-	if (!node) node = ConsumeBackreference(tokens, tokenCount, position);
-	if (!node) {
-		free(node);
-		*position = startPos;
-		return NULL;
+	if (Peek(tokens, position)->type == TOKEN_WILDCARD) {
+		node->value = Consume(TOKEN_WILDCARD, tokens, position);
+		node->type = NODE_WILDCARD;
 	}
 	return node;
 }
@@ -252,17 +201,68 @@ ASTNode *ConsumeMatch(const Token* tokens, int tokenCount, int *position) {
 	return node;
 }
 
-ASTNode *ConsumeMatchItem(const Token* tokens, int tokenCount, int *position) {
+ASTNode *ConsumeSubExpressionItem(const Token* tokens, int tokenCount, int *position) {
 	int startPos = *position;
 	if (*position >= tokenCount) {
 		return NULL;
 	}
 	ASTNode *node = malloc(sizeof(ASTNode));
-	if (Peek(tokens, position).type = TOKEN_WILDCARD) {
-		node->value = Consume(TOKEN_WILDCARD, tokens, position);
-		node->type = NODE_WILDCARD;
+	node = ConsumeMatch(tokens, tokenCount, position);
+	if (!node) node = ConsumeGroup(tokens, tokenCount, position);
+	if (!node) node = ConsumeAnchor(tokens, tokenCount, position);
+	if (!node) node = ConsumeBackreference(tokens, tokenCount, position);
+	if (!node) {
+		free(node);
+		*position = startPos;
+		return NULL;
 	}
 	return node;
+}
+
+ASTNode *ConsumeSubExpression(const Token* tokens, int tokenCount, int *position) {
+	int startPos = *position;
+	if (*position >= tokenCount) {
+		return NULL;
+	}
+	ASTNode *node = malloc(sizeof(ASTNode));
+	node->type = NODE_SUBEXPRESSION;
+	node->left = ConsumeSubExpressionItem(tokens, tokenCount, position);
+	if (!node->left) {
+		free(node);
+		*position = startPos;
+		return NULL;
+	}
+	node->right = ConsumeSubExpression(tokens, tokenCount, position);
+	return node;
+}
+
+ASTNode *ConsumeExpression(const Token* tokens, int tokenCount, int *position) {
+	int startPos = *position;
+	if (*position >= tokenCount) {
+		return NULL;
+	}
+	ASTNode *node = malloc(sizeof(ASTNode));
+	node->type = NODE_EXPRESSION;
+	node->left = ConsumeSubExpression(tokens, tokenCount, position);
+	if (!node->left) {
+		free(node);
+		*position = startPos;
+		return NULL;
+	}
+	if (Peek(tokens, position)->type == TOKEN_ALTERNATOR) {
+		Consume(TOKEN_ALTERNATOR, tokens, position);
+		node->right = ConsumeExpression(tokens, tokenCount, position);
+	}
+	return node;
+}
+
+ASTNode *Parse(const Token* tokens, int tokenCount) { //shoutout https://github.com/kean/Regex/blob/main/grammar.ebnf
+	const Token* token = tokens;
+	ASTNode *regex = malloc(sizeof(ASTNode));
+	int position = 0;
+	regex->type = NODE_REGEX;
+	regex->left = ConsumeExpression(tokens, tokenCount, &position);
+	return regex;
 }
 
 int main() {
